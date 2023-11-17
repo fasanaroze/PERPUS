841ae953ad8e27f8deb031fcca2972a2921fbc8b
SalsabilaYulianti <141704344+SalsabilaYulianti@users.noreply.github.com>
2023-11-17 10:49:28 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+      <div class="nav-container">
+          <img src="@/assets/img/smk.png" class="logosmk"/>
+          <div class="headerText">
+              <h1>PERPUS DIGITAL</h1>
+          </div>
+          <h4>SMKN 4 TASIKMALAYA</h4>
+          <img src="@/assets/img/f.png" class="logoclub"/>
+          
+          
+      </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+<style scoped>
+*{
+margin: 0;
+z-index: 1;
+}
+.nav-container {
+  background-color:#1e1e1e;
+  width: 100%;
+  height: 150px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+
+.logoclub {
+  width: 100px;
+  height: 90px;
+}
+
+.logosmk{
+  width: 111px;
+  height: 126px;
+
+}
+h1 {
+  color: #ffffff;
+  text-align: right;
+  font-family: '';
+  font-size: 20px;
+  letter-spacing: 9px;
+  font-weight: 50;
+  line-height: normal;
+
+}
+
+h4{
+  color: #ffffff;
+  text-align: right;
+  font-family: '';
+  font-size: 20px;
+  letter-spacing: 9px;
+  font-weight: 50;
+  line-height: normal;
+}
+
+@media (max-width: 520px) {
+  .nav-container {
+      background-color:#1e1e1e;
+      width: 100%;
+      height: 80px;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+  }
+  
+  .logosmk {
+      width: 150px;
+      height:100px;
+  }
+  
+  }
+  .logosmk{
+      width: 46.077px;
+      height: 50px;
+  
+  }
+  h1 {
+      color: #ffffff;
+      text-align: right;
+      font-family: '';
+      font-size: 26px;
+      letter-spacing: 4px;
+      font-style: 'light';
+  
+  }
+  
+  h4{
+    color: #ffffff;
+      text-align: right;
+      font-family: '';
+      font-size: 26px;
+      letter-spacing: 4px;
+      font-style: 'light';
+  
+  }
+
+@media (max-width: 820px) {
+  .nav-container {
+      background-color:#1e1e1e;
+      width: 100%;
+      height: 130px;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+  }
+  
+  .logoclub {
+      width: 100px;
+      height:118px;
+  }
+  
+  
+  .logosmk{
+      width: 80.077px;
+      height: 80px;
+  
+  }
+  h1 {
+      color: #1e1e1e;
+      text-align: center;
+      font-family: '';
+      font-size: 15px;
+      letter-spacing: 2px;
+      font-style: 'light';
+  
+  }
+  
+  h4{
+    color: #1e1e1e;
+      text-align: center;
+      font-family: '';
+      font-size: 15px;
+      letter-spacing: 2px;
+      font-style: 'light';
+  
+  }
+}
+</style>
---
+++ components/kunjungan.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2 class="text-center">Isi Buku Kunjungan</h2>
+  <br />
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Nama</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" />
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Kategori</label>
+    <select class="form-select" id="specificSizeSelect">
+      <option value="Siswa">Siswa</option>
+      <option value="Guru">Guru</option>
+      <option value="Staf">Staf</option>
+    </select>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" />
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Keperluan</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" />
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">SUBMIT</button>
+</template>
